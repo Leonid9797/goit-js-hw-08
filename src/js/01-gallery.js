abcf413e2707c2249725d8c875424d38e6335b30
Leonid9797 <123494748+Leonid9797@users.noreply.github.com>
2023-06-08 12:32:31 +0200

    upp

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryConteiner = document.querySelector(`.gallery`);
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryConteiner.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+galleryConteiner.addEventListener('click', onGalleryConteinerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description } = {}) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>
+    `;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function onGalleryConteinerClick(evt) {
+  evt.preventDefault();
+}
+
+const styleLi = document.querySelector('ul');
+styleLi.style.listStyleType = 'none';
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-pleyer');
+const throttleUpdate = _.throttle(updateCurrentTime, 1000);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('play', function () {
+  throttleUpdate();
+});
+
+player.on('timeupdate', function () {
+  throttleUpdate();
+});
+
+function updateCurrentTime() {
+  const currentTime = player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}
