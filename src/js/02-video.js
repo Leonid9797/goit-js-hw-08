283b56c258e50c63546ef246ca1886897d0c8565
Leonid9797 <123494748+Leonid9797@users.noreply.github.com>
2023-06-08 12:33:09 +0200

    ipo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-pleyer');
+
 const throttleUpdate = _.throttle(updateCurrentTime, 1000);
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
